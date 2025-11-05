5ce9b9f5a978a7aafd66da8f6bb05ca5ba7788d3
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-11-05 15:50:35 +0000

    features

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,4 @@
-// import { useEffect } from "react";
-// import Scheduler from "./components/Scheduler";
-// import GitHubFooter from "./components/GitHubFooter";
-// import "./App.css";
 
-// function App() {
-//   useEffect(() => {
-//     document.title = "CPU Scheduler";
-//   }, []);
-//   return (
-//     <div className="bg-black">
-//         <div className="m-auto p-6 bg-black">
-//         <Scheduler />
-//         </div>
-//         <GitHubFooter />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// updated
 
 import { useEffect } from "react";
 import Scheduler from "./components/Scheduler";
